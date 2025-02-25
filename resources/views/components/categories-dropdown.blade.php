6ea10818068783b08964c7849a617d4d377b5cf2
ahed666 <ahed0naser0eddin@gmail.com>
2025-02-25 15:27:54 +0400

    edit categories dropdown

--- resources/views/components/categories-dropdown.blade.php
+++ resources/views/components/categories-dropdown.blade.php
@@ -5,12 +5,22 @@
   <a class="nav-link dropdown-toggle " href="#" id="categoriesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     {{$title}}
   </a>
+  
   <div class="dropdown-menu" id="categoriesDropdownMenu" aria-labelledby="categoriesDropdown">
+    @if($type=="brand")
+    @foreach($items as $item)
+    <a href="{{route($type.'.show', $item->slug) }}" class="dropdown-item">
+     {{$item->brand_name}}
+    </a>
+    @endforeach
+    @else
+
     @foreach($items as $item)
     <a href="{{route($type.'.show', $item->slug) }}" class="dropdown-item">
      {{$item->name}}
     </a>
     @endforeach
+    @endif
   </div>
 </li>
 

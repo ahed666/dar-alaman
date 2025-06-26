eb8a0719bbaab8f6ec8a4e00060a92a2d30ae69d
ahed666 <ahed0naser0eddin@gmail.com>
2025-06-26 14:10:20 +0400

    edit box image

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -21,6 +21,12 @@ used '.$product->name.' UAE' )
 @section('t_image', Voyager::image($product->main_image) )
 
 @section('content')
+<style>
+  .pswp__img {
+    object-fit: contain !important;
+    
+  }
+</style>
 <div class="container-fluid page-header py-6 wow fadeIn" data-wow-delay="0.1s">
     <div class="container text-center pt-5 pb-3">
         <h1 class="display-4 text-white animated slideInDown mb-3">{{$product->name}}</h1>
